0f831c936da62bc6e5b64c370c7c91bf163d7bfc
Fangzhou Jiang <fangzhoujiang617@gmail.com>
2026-08-26 18:49:57 +0800

    ci: add github pages deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // 开启 Next.js 静态网页导出功能
+  images: {
+    unoptimized: true, // 静态导出需关闭默认图片优化引擎
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
